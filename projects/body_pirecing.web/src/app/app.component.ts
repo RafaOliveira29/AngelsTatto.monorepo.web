6e7ba7e6f95d7484ce39fdbeb5eb9b59824bc916
RafaOliveira29 <rafael.osilva2929@gmail.com>
2024-08-02 15:38:28 -0300

    develop - removendo código desnecessario de body_pirecing

--- projects/body_pirecing.web/src/app/app.component.ts
+++ projects/body_pirecing.web/src/app/app.component.ts
@@ -10,18 +10,4 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'body_pirecing.web';
-
-  highlightTextPresentationTitle(text: string, color: string): string {
-    const regex = /(BODYPIERCING|GELCO PRO)/gi;
-    return text.replace(regex, `<span style="color: ${color};">$&</span>`);
-  }
-
-  highlightTextPresentationSubtitle(text: string, color: string): string {
-    const regex = /(você aprende tudo|profissional completo)/gi;
-    return text.replace(regex, `<span style="color: ${color};">$&</span>`);
-  }
-
-  onClick() {
-    console.log('CLICOU E FUNCIONOU');
-  }
 }
