92978e5e46b2c0d1f0eea36b8cf5613711667c60
RafaOliveira29 <91383247+RafaOliveira29@users.noreply.github.com>
2024-08-10 15:05:05 -0300

    feat/firebase-hosting - hospedando sites

---
+++ projects/tattoo.web/src/app/environments/environments.ts
@@ -0,0 +1,11 @@
+export const environments = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyB4ppScsI9WbKMrK2ctjeFpVOR3IV-_1wc',
+    authDomain: 'angelstattoo-a6408.firebaseapp.com',
+    projectId: 'angelstattoo-a6408',
+    storageBucket: 'angelstattoo-a6408.appspot.com',
+    messagingSenderId: '1074244640696',
+    appId: '1:1074244640696:web:3b3840fc8826eaa5e6794d',
+    measurementId: 'G-C1C67G8LBJ',
+  },
+};
